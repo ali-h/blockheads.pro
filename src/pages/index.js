614657d8ca6de916dc9a06e938c81c9306c688a9
Ali H <34513931+ali-h@users.noreply.github.com>
2024-06-20 22:34:24 +0500

    contact fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,12 +40,12 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/contact"
+                  href="/about"
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border border-solid border-transparent hover:border-dark
                 dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base
                 "
                 >
-                  Explore <LinkArrow className={"w-6 ml-1"} />
+                  Contact <LinkArrow className={"w-6 ml-1"} />
                 </Link>
               </div>
             </div>
